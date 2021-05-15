dca4e3bca1b23460f4eb951347e6993648e5e17d
Dell <parmgill59522@gmail.com>
2021-05-15 21:43:22 +0530

    initial---commit

---
+++ factorial.py
@@ -0,0 +1,21 @@
+def main():
+    InputData()
+    res()
+def InputData():
+    number = int(input("Enter any positive number: "))
+    if int(number) > 0:
+        number = int(number)
+        calcFactorial(number)
+    else:
+        print("The number that you entered is invalid.")
+        exit()
+
+def calcFactorial(number):
+    global Factorial
+    Factorial = 1
+    while number > 0:
+        Factorial = Factorial * number
+        number = number - 1
+def res():
+    print(f' Factorial of the number is  { Factorial }')
+main()
